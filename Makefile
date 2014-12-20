164b7e7012ce971e8fd88ae039ff83d49e11d646
izabera <izaberina@gmail.com>
2014-12-20 12:34:08 +0100

    temp

--- Makefile
+++ Makefile
@@ -1,11 +1,20 @@
 all:
-	gcc -std=c99 -Wall -O3 -s -march=native -o bigrep bigrep.c
+	gcc -std=c99                     -pedantic -Wall -Wextra -O2 -march=native -o bigrep bigrep.c
 
 human:
-	gcc -std=c99 -DHUMAN_READABLE -Wall -O3 -s -march=native -o bigrep bigrep.c
+	gcc -std=c99 -DHUMAN_READABLE    -pedantic -Wall -Wextra -O2 -march=native -o bigrep bigrep.c
 
 debug:
-	gcc -std=c99 -DHUMAN_READABLE -g -Wall -o bigrep bigrep.c
+	gcc -std=c99 -DHUMAN_READABLE -g -pedantic -Wall -Wextra                   -o bigrep bigrep.c
 
 debug-optimized:
-	gcc -std=c99 -DHUMAN_READABLE -g -Wall -O3 -march=native -o bigrep bigrep.c
+	gcc -std=c99 -DHUMAN_READABLE -g -pedantic -Wall -Wextra -O2 -march=native -o bigrep bigrep.c
+
+clean:
+	rm -f bigrep
+
+install:
+	install -m 0755 bigrep /usr/local/bin/bigrep
+
+uninstall:
+	rm -f /usr/local/bin/bigrep
--- bigrep.c
+++ bigrep.c
@@ -18,6 +18,7 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 
@@ -29,34 +30,40 @@
 #define NOT_FOUND       1
 #define FOUND           0
 
-bool substringfound (FILE *filea, FILE *fileb) {
+unsigned char *buffera, *bufferb;
 
-  /* if a variable size buffer is used, the problem is O(n)
-   * otherwise it's between O(n) and O(n*n)
-   * TODO: add an option for this */
-#define BUFFER_SIZE 4096
-  unsigned char buffera[BUFFER_SIZE], bufferb[BUFFER_SIZE];
+bool substringfound (FILE *filea, FILE *fileb, unsigned int BUFFER_SIZE) {
 
   size_t size_filea, size_fileb;
+  bool ret;
   while (true) {
     size_filea = fread(buffera, sizeof(unsigned char), BUFFER_SIZE, filea);
     size_fileb = fread(bufferb, sizeof(unsigned char), BUFFER_SIZE, fileb);
 
-    if (memcmp(buffera, bufferb, size_filea) != 0) return false;
+    if (memcmp(buffera, bufferb, size_filea) != 0) {
+      ret = false;
+      break;
+    }
     /* if we're at eof in filea, we won */
-    if (size_filea < BUFFER_SIZE) return true;
+    if (size_filea < BUFFER_SIZE) {
+      ret = true;
+      break;
+    }
     /* we're not at eof in filea, but we're at eof in fileb, we lost */
-    if (size_fileb < BUFFER_SIZE) return false;
+    if (size_fileb < BUFFER_SIZE) {
+      ret = false;
+      break;
+    }
   }
-  /*return something_went_wrong;*/
+  return ret;
 }
 
 int main (int argc, char ** argv) {
   FILE *filea, *fileb;
-  size_t offset, size_fileb;
+  size_t offset, size_filea, size_fileb;
   bool found = false;
 
-  unsigned int i, ret = INITIAL_STATUS;
+  int i, ret = INITIAL_STATUS;
 
   /* human readable errors are printed to stderr
    * error numbers are written to stdout to provide and easy to parse interface
@@ -79,6 +86,25 @@ int main (int argc, char ** argv) {
 #endif
     return CANT_OPEN_FILEA;
   }
+  fseek(filea, 0, SEEK_END);
+  size_filea = ftell(filea);
+
+  /* if a variable size buffer is used, the problem is O(n)
+   * otherwise it's between O(n) and O(n*n) */
+#ifdef STATIC_BUFFER
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 4096
+#endif
+#else
+  unsigned int BUFFER_SIZE = size_filea;
+#endif
+
+  buffera = (unsigned char*) malloc(BUFFER_SIZE * sizeof(unsigned char));
+  bufferb = (unsigned char*) malloc(BUFFER_SIZE * sizeof(unsigned char));
+
+#ifdef STATIC_BUFFER
+  fread(buffera, sizeof(unsigned char), BUFFER_SIZE, filea);
+#endif
 
   for (i = 2; i < argc; i++) {
     fileb = fopen(argv[i], "rb");
@@ -95,11 +121,13 @@ int main (int argc, char ** argv) {
       found = false;
       fseek(fileb, 0, SEEK_END);
       size_fileb = ftell(fileb);
-      for (offset = 0; offset < size_fileb; offset++) {
-        rewind(filea);
-        fseek(fileb, offset, SEEK_SET);
-        found = substringfound(filea, fileb);
-        if (found) break;
+      if (size_filea <= size_fileb) {
+        for (offset = 0; offset <= size_fileb - size_filea; offset++) {
+          rewind(filea);
+          fseek(fileb, offset, SEEK_SET);
+          found = substringfound(filea, fileb, BUFFER_SIZE);
+          if (found) break;
+        }
       }
 
       if (!found) {
@@ -122,6 +150,8 @@ int main (int argc, char ** argv) {
     }
   }
   fclose(filea);
+  free(buffera);
+  free(bufferb);
   return ret;
 }
 
